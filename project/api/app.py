dd34ecfc5e9bb49c70de3ad14ea1854eec3bbeb9
Alper <Alperhakli2004@gmail.com>
2026-02-03 19:41:31 +0300

    middleware added
    middeware added

--- project/api/app.py
+++ project/api/app.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from project.api.routes import analysis_route , stocks_route
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def initialize_api():
     return {"api_message" : "Finpolar api initialized"}
+
 
 
 app.include_router(analysis_route.router)
